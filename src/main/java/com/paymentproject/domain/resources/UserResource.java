fafaeb32512c7a73b6d21841b9aee6aead65f971
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-01-07 17:57:19 -0300

    UserResource created

---
+++ src/main/java/com/paymentproject/domain/resources/UserResource.java
@@ -0,0 +1,50 @@
+package com.paymentproject.domain.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paymentproject.domain.dtos.UserDTO;
+import com.paymentproject.domain.entities.User;
+import com.paymentproject.domain.services.UserService;
+import com.paymentproject.domain.services.exceptions.DatabaseException;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+    
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody UserDTO userDTO){
+        try {
+            return new ResponseEntity<>(userService.save(userDTO), HttpStatus.CREATED);
+        } catch (Exception e){
+            throw new DatabaseException(null);
+        }
+        
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> userList = userService.findAll();
+
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
