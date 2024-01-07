0a2457e9c19907f48dd673516e488140d84b7d65
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-01-07 14:39:24 -0300

    transaction created

---
+++ src/main/java/com/paymentproject/domain/entities/Transaction.java
@@ -0,0 +1,40 @@
+package com.paymentproject.domain.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_transaction")
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="reciever_id")
+    private User reciever;
+    private LocalDateTime timestamp;
+    
+}
