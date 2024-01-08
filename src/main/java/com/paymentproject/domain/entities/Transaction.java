b6d783fb6c1fd4a392982703fba71576131f9ad3
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-01-08 11:09:02 -0300

    added NoArgsConstructor to transaction entity

--- src/main/java/com/paymentproject/domain/entities/Transaction.java
+++ src/main/java/com/paymentproject/domain/entities/Transaction.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(of="id")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
 
     @Id
