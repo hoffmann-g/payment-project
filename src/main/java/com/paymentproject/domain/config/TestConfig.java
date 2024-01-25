7b582f4864b899753f32831b5445b85a1ac39b72
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-01-25 18:11:20 -0300

    TestConfig now utilizes authenticationService

--- src/main/java/com/paymentproject/domain/config/TestConfig.java
+++ src/main/java/com/paymentproject/domain/config/TestConfig.java
@@ -1,21 +1,25 @@
 package com.paymentproject.domain.config;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.paymentproject.domain.entities.Transaction;
 import com.paymentproject.domain.entities.User;
-import com.paymentproject.domain.respositories.UserRepository;
+import com.paymentproject.domain.respositories.TransactionRepository;
+import com.paymentproject.domain.services.AuthenticationService;
 import com.paymentproject.domain.entities.enums.UserType;
 
 @Configuration
 public class TestConfig implements CommandLineRunner {
     
     @Autowired
-    private UserRepository userRepository;
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,7 +28,13 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Maria", "Dunha", "68962061015", "maria@gmail.com", "password123", new BigDecimal(50), UserType.COMMON );
         User u3 = new User(null, "Mr.", "Cadela", "97842257000", "cadela@sigmamales.com", "password123", new BigDecimal(6000), UserType.COMMON );
 
-        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+        authenticationService.register(u1);
+        authenticationService.register(u2);
+        authenticationService.register(u3);
+
+        Transaction transaction = new Transaction(null, new BigDecimal(30.), u1, u2, LocalDateTime.now());
+
+        transactionRepository.save(transaction);
     }
 
 
