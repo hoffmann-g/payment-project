77ee0bb1e8b3b7a05f3b39169a6e758c240cb554
Guilherme Hoffmann <g.hoffmann.hbs@gmail.com>
2024-01-25 18:11:37 -0300

    User now implements UserDetails

--- src/main/java/com/paymentproject/domain/entities/User.java
+++ src/main/java/com/paymentproject/domain/entities/User.java
@@ -1,6 +1,12 @@
 package com.paymentproject.domain.entities;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.paymentproject.domain.entities.enums.UserType;
 
@@ -26,11 +32,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class User {
+public class User implements UserDetails{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String firstName;
     private String lastName;
@@ -40,11 +47,51 @@ public class User {
 
     @Column(unique = true)
     private String email;
+
     private String password;
     private BigDecimal balance;
     
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        switch(this.userType){
+            case MERCHANT:
+                return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+            default:
+                return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getUsername() {
+        return document;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+        return true;
+    }
     
 }
 
